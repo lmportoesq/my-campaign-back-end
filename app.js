4bd42ad11c7f42590f9b3581a38a234610f854ef
Luis porto <lmportoesq@gmail.com>
2022-05-26 16:23:26 -0500

    feac/asign users routes

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const configExpress = require('./api/config/express');
 const connectDB = require('./api/config/database');
+const routes = require('./routes');
 
 const app = express();
 connectDB();
 
 configExpress(app);
+routes(app);
 
 const port = process.env.PORT || 8080;
 app.listen(port,()=>{
