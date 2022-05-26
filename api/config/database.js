fe61efb9ffb9a23182f7f83bb4d82ae7eab9cf1e
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-05-26 15:24:37 -0500

    feac: install mongoose and connect to DB (#3)
    Co-authored-by: Luis porto <lmportoesq@gmail.com>

---
+++ api/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const uri = process.env.MONGO_DB_URI;
+
+async function connectDB(){
+    try {
+        await mongoose.connect(uri);
+        console.log(`Mongoose is connected to ${uri}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+module.exports = connectDB;
