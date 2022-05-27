3a0602a57b9957bc14b83a6bc1874d3322f38e19
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-05-26 19:37:48 -0500

    Feac/create crud users (#4)
    * feac: create structure for users and routes
    
    * feac/create user model
    
    * feac/create user services
    
    * feac/create user controller
    
    * feac/asign users routes
    
    * fix: imports
    
    * feac: create user's CRUD
    
    Co-authored-by: Luis porto <lmportoesq@gmail.com>

---
+++ api/users/index.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const {
+  handlerCreateUser,
+  handlerGetAllUsers,
+  handlerGetOneUser,
+  handlerUpdateUser,
+  handlerDeleteUser,
+} = require('./user.controller');
+
+//const { isAuthenticated } = require('../../auth/auth.service');
+const router = Router();
+router.post('/', handlerCreateUser);
+
+router.get('/', handlerGetAllUsers);
+router.get('/:id', handlerGetOneUser);
+router.patch('/:id', handlerUpdateUser);
+//router.delete('/:id', isAuthenticated(), handlerDeleteUser);
+router.delete('/:id', handlerDeleteUser);
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
-require('dotenv').config();
+
 const configExpress = require('./api/config/express');
 const connectDB = require('./api/config/database');
+const routes = require('./routes');
+require('dotenv').config();
 
 const app = express();
-connectDB();
 
+connectDB();
 configExpress(app);
+routes(app);
 
 const port = process.env.PORT || 8080;
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
 });
+
+module.exports = app;
