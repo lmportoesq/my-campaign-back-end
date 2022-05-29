3c7c201689e2ad7c4d81abdd0076f5785c7f9fa7
Luis porto <lmportoesq@gmail.com>
2022-05-29 11:52:53 -0500

    Feac: create structure

---
+++ api/campaigns/index.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const {
+  handlerCreateCampaign,
+  handlerGetAllCampaigns,
+  handlerGetOneCampaign,
+  handlerUpdateCampaign,
+  handlerDeleteCampaign,
+} = require('./campaign.controller');
+
+const { isAuthenticated } = require('../../auth/auth.service');
+const router = Router();
+router.post('/', );
+
+router.get('/', handlerGetAllCampaigns);
+router.get('/:id', handlerGetOneCampaign);
+router.patch('/:id', handlerUpdateCampaign);
+router.delete('/:id', isAuthenticated(), handlerDeleteCampaign);
+
+module.exports = router;
