5e6319d43b0acc8c30598ac616b71d70e845d5c4
Luis porto <lmportoesq@gmail.com>
2022-06-01 17:55:15 -0500

    Feac: add leaders route

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 const req = require('express/lib/request');
 const users = require('./api/users');
+const leaders = require('./api/leaders');
 campaigns = require('./api/campaigns')
 const authLocal = require('./auth/local');
 
 function routes(app){
   app.use('/api/users', users);
+  app.use('/api/leader', leaders);
   app.use('/api/campaigns', campaigns);
   app.use('/auth/local', authLocal);
 }
