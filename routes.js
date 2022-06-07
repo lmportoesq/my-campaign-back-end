a4e73d81096501777b4206b3e94fb7e5c951e4ac
Luis porto <lmportoesq@gmail.com>
2022-06-07 08:47:09 -0500

    feac: create routes to rejecteds

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const req = require('express/lib/request');
 const users = require('./api/users');
 const leaders = require('./api/leaders');
 const followers = require('./api/followers');
+const rejecteds = require('./api/rejecteds');
 campaigns = require('./api/campaigns')
 const authLocal = require('./auth/local');
 
@@ -9,6 +10,7 @@ function routes(app){
   app.use('/api/users', users);
   app.use('/api/leaders', leaders);
   app.use('/api/followers', followers);
+  app.use('/api/rejecteds', rejecteds);
   app.use('/api/campaigns', campaigns);
   app.use('/auth/local', authLocal);
 }
