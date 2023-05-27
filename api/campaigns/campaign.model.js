576743992482e49559d3bf76107535d7783ce9d5
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2023-05-27 16:42:28 -0500

    Update campaign.model.js
    Se elimina campaignAdress que se encontraba duplicado.

--- api/campaigns/campaign.model.js
+++ api/campaigns/campaign.model.js
@@ -20,9 +20,6 @@ const CampaignSchema = new mongoose.Schema({
   campaignLogo: {
     type: String,
   },
-  campaignAdress: {
-    type: String,
-  },
 }, {
   timestamps: true,
   versionKey: false,
